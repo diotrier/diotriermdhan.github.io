feda6e0d9db9b7a0f93b4029e79ff8c9bd51cfad
diotrier <ramadhantrie9@gmail.com>
2023-09-04 20:22:10 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// toggle
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+//click on sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
